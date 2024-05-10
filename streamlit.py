2408ba2cba351094d109b0fc57462f1d33c180cf
Vincent Benedict Mars W <114089447+vincentbmw@users.noreply.github.com>
2024-05-10 11:58:02 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,7 +4,7 @@ import subprocess
 def main():
     st.write("Running Bot...")
     try:
-        subprocess.run(["python", "Bounty/app.py"])
+        subprocess.run(["python", "-m", "Bounty/app.py"])
     except FileNotFoundError:
         st.error("File 'Bounty/app.py' not found.")
 
